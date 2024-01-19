2b95638f301e42f4838ba494a58154f84f414488
picoloman6 <a2328b@naver.com>
2024-01-19 19:44:51 +0900

    [comment] redux 파일들에 주석으로 설명

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -8,13 +8,16 @@ const rootReducer = combineReducers({
   todos: todoReducer
 });
 
+// 최상위 컴포넌트에서 사용
 const store = configureStore({
   reducer: rootReducer
 });
 
+// useSelector와 useDispatch의 타입
 type RootState = ReturnType<typeof store.getState>;
 type AppDispatch = typeof store.dispatch;
 
+// 타입을 적용한 useSelector와 useDispatch
 export const useAppSelector = useSelector.withTypes<RootState>();
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 
--- src/modules/todolist.ts
+++ src/modules/todolist.ts
@@ -1,27 +1,34 @@
 import { TodosTypes } from '../components/TodoList/TodosTypes';
 
+// 상태 타입
 interface StateTypes {
   todos: TodosTypes[];
 }
 
+// 리듀서 액션 인수 타입 - A
 interface SetTodosTypes {
   type: 'todolist/SET_ITEMS';
   todos: TodosTypes[];
 }
 
+// 리듀서 액션 인수 타입 - B
 type ActionTypes = SetTodosTypes;
 
+// 1. 액션 타입 정의
 const SET_ITEMS = 'todolist/SET_ITEMS';
 
+// 2. 액션 객체 생성 함수
 export const setTodoslist = (todos: TodosTypes[]) => ({
   type: SET_ITEMS,
   todos
 });
 
+// 3. 초기 상태
 const initialState: StateTypes = {
   todos: []
 };
 
+// 4. 리듀서 함수
 const todoReducer = (state = initialState, action: ActionTypes) => {
   switch (action.type) {
     case SET_ITEMS:
